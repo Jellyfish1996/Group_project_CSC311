fd99f2ee2746ed7eabbe879dc3d139e12d5cb0a8
wyattanderson <wyatt.anderson@mail.utoronto.ca>
2025-08-05 17:04:22 -0400

    commiting new code

--- ProjectDefault-starter-files-ande1002/item_response.py
+++ ProjectDefault-starter-files-ande1002/item_response.py
@@ -5,6 +5,7 @@ from utils import (
     load_train_sparse,
 )
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
@@ -28,6 +29,18 @@ def neg_log_likelihood(data, theta, beta):
     # Implement the function as described in the docstring.             #
     #####################################################################
     log_lklihood = 0.0
+    for i in range(len(data["user_id"])):
+        u = data["user_id"][i]
+        q = data["question_id"][i]
+        c = data["is_correct"][i]
+
+        z = theta[u] - beta[q]
+
+        if c == 1:
+            log_lklihood -= np.logaddexp(0, -z)
+        else:
+            log_lklihood -= np.logaddexp(0, z)
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -55,7 +68,23 @@ def update_theta_beta(data, lr, theta, beta):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    pass
+
+    d_theta = np.zeros_like(theta)
+    d_beta = np.zeros_like(beta)
+
+    for i in range(len(data["user_id"])):
+        u = data["user_id"][i]
+        q = data["question_id"][i]
+        c = data["is_correct"][i]
+
+        z = theta[u] - beta[q]
+        p = sigmoid(z)
+
+        d_theta[u] += c - p
+        d_beta[q] += p - c
+
+    theta += lr * d_theta
+    beta += lr * d_beta
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -76,20 +105,27 @@ def irt(data, val_data, lr, iterations):
     :return: (theta, beta, val_acc_lst)
     """
     # TODO: Initialize theta and beta.
-    theta = None
-    beta = None
+    num_users = max(data["user_id"]) + 1  # assuming zero-based IDs
+    num_questions = max(data["question_id"]) + 1
+    theta = np.random.normal(0, 1, size=num_users)
+    beta = np.random.normal(0, 1, size=num_questions)
 
     val_acc_lst = []
+    train_lld_lst = []
+    val_lld_lst = []
 
     for i in range(iterations):
         neg_lld = neg_log_likelihood(data, theta=theta, beta=beta)
+        train_lld_lst.append(neg_lld)
         score = evaluate(data=val_data, theta=theta, beta=beta)
         val_acc_lst.append(score)
+        val_lld = neg_log_likelihood(val_data, theta, beta)
+        val_lld_lst.append(val_lld)
         print("NLLK: {} \t Score: {}".format(neg_lld, score))
         theta, beta = update_theta_beta(data, lr, theta, beta)
 
     # TODO: You may change the return values to achieve what you want.
-    return theta, beta, val_acc_lst
+    return theta, beta, val_acc_lst, train_lld_lst, val_lld_lst
 
 
 def evaluate(data, theta, beta):
@@ -122,16 +158,65 @@ def main():
     # Tune learning rate and number of iterations. With the implemented #
     # code, report the validation and test accuracy.                    #
     #####################################################################
-    pass
+    train_lls = []
+    val_lls = []
+    val_accs = []
+
+    lr = 0.001
+    # iterations = np.arange(0, 900)
+
+    # theta, beta, val_acc_lst,train_lld_lst , val_lld_lst = irt(train_data, val_data,lr,900)
+
+    # plt.plot(iterations, train_lld_lst, label="Training Log-Likelihood")
+    # plt.plot(iterations,val_lld_lst, label = "Validation Log-Likelihood")
+    # plt.xlabel("Iteration")
+    # plt.ylabel("Log-Likelihood")
+    # plt.title("Training Curve for Number of Iterations")
+    # plt.legend()
+    # plt.show()
+
+    # plt.plot(iterations, val_acc_lst, label="Validation Accuracy")
+    # plt.xlabel("Iteration")
+    # plt.ylabel("Validation Score")
+    # plt.title("Validation Scores for Number of Iterations")
+    # plt.legend()
+    # plt.show()
+
+    # final hyperparameters and test
+    theta, beta, val_acc_lst, train_lld_lst, val_lld_lst = irt(train_data, val_data, lr, 500)
+    final_val_score = val_acc_lst[-1]
+    test_score = evaluate(test_data, theta, beta)
+    print("Test score: ", test_score)
+    print("Final Validation score:  ", final_val_score)
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
 
     #####################################################################
     # TODO:                                                             #
-    # Implement part (d)                                                #
+    # Implement part (d)
+    #
     #####################################################################
-    pass
+
+    thetas = []
+    probs = []
+    theta_range = np.linspace(-3, 3, 100)
+
+    for i in range(3):
+        u = test_data["user_id"][i]
+        q = test_data["question_id"][i]
+        c = test_data["is_correct"][i]
+
+        probs = [sigmoid(t - beta[q]) for t in theta_range]
+        plt.plot(theta_range, probs, label=f"Question {q}")
+
+    plt.xlabel("Theta (student ability)")
+    plt.ylabel("Probability of Correct Response")
+    plt.title("IRT Item Characteristic Curves")
+    plt.legend()
+    plt.show()
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
--- ProjectDefault-starter-files-ande1002/matrix_factorization.py
+++ ProjectDefault-starter-files-ande1002/matrix_factorization.py
@@ -1,7 +1,11 @@
 import matplotlib
+import skopt
 from scipy.linalg import sqrtm
 import matplotlib.pyplot as plt
 import numpy as np
+import item_response
+from skopt.space import Real, Integer
+from skopt.plots import plot_convergence, plot_evaluations
 matplotlib.use('TkAgg')
 
 from utils import (
@@ -146,6 +150,56 @@ def als(train_data, k, lr, num_iteration):
     #                       END OF YOUR CODE                            #
     #####################################################################
     return mat
+def als_with_modification(train_data, k, lr, num_iteration, theta, beta):
+    """Performs ALS algorithm, here we use the iterative solution - SGD
+    rather than the direct solution.
+
+    :param train_data: A dictionary {user_id: list, question_id: list,
+    is_correct: list}
+    :param k: int
+    :param lr: float
+    :param num_iteration: int
+    :return: 2D reconstructed Matrix.
+    """
+    # Initialize u and z
+    u = np.random.uniform(
+        low=0, high=1 / np.sqrt(k), size=(len(set(train_data["user_id"])), k)
+    )
+    z = np.random.uniform(
+        low=0, high=1 / np.sqrt(k), size=(len(set(train_data["question_id"])), k)
+    )
+
+    #####################################################################
+    #                                                      #
+    # Implement the function as described in the docstring.             #
+    #####################################################################
+
+
+    for it in range(num_iteration):
+        i = np.random.choice(len(train_data["user_id"]))
+        user_id = train_data["user_id"][i]
+        question_id = train_data["question_id"][i]
+        score = train_data["is_correct"][i]
+
+        pred = theta[user_id] + beta[question_id] + np.dot(u[user_id], z[question_id])
+        error = score - pred
+
+        u_old = u[user_id].copy()
+
+        u[user_id] += lr * error * z[question_id]
+        z[question_id] += lr * error * u_old
+
+        theta[user_id] += lr * error
+        beta[question_id] += lr * error
+
+    # Reconstruct the final prediction matrix
+    final_predictions = theta[:, np.newaxis] + beta[np.newaxis, :] + np.dot(u, z.T)
+    return final_predictions
+
+    #####################################################################
+    #                       END OF YOUR CODE                            #
+    #####################################################################
+    return mat
 
 
 def als_with_tracking(train_data, val_data, k, lr, num_iterations):
@@ -179,110 +233,210 @@ def als_with_tracking(train_data, val_data, k, lr, num_iterations):
     return mat, train_losses, val_losses
 
 
+def helper_evaluate(k, lr, num_iter, train_data, val_data):
+    """
+    :param train_data: The training data used to evaluate training accuracy based on
+    hyper_parameters
+    :param k: int
+    :param lr: float
+    :param num_iter: int
+    :return: A float representing negative accuracy (gp minimize minimizes accuracy)
+    """
+
+    accuracy = sparse_matrix_evaluate(val_data, als(train_data, k, lr, num_iter))
+    return -accuracy
+
+
+def find_optimal_hyperparameters(search_space, iterations, train_data, val_data):
+    """
+     Designed to predict optimal hyperparameters using Bayesian Optimization.
+
+    :param search_space: A list of tuples, each specifying the range of values to be considered
+    for a specific hyperparameter, e.g. [Integer(10, 100, name='k'), Real(0.1, 1.0, name='lr')]
+    :param iterations: Number of iterations to preform
+    :param train_data: The training data
+    returns:
+        res : OptimizeResult, a scipy object
+            The optimization result returned as an OptimizeResult object.
+            Important attributes are:
+            x : list
+                The location of the minimum (i.e., the best hyperparameter values found).
+            fun : float
+                The function value at the minimum (i.e., the best score).
+            x_iters : list of lists
+                The location of function evaluation for each iteration (the hyperparameter values tried).
+            func_vals : array
+                The function value for each iteration (the scores achieved for each trial).
+            space : Space
+                The optimization space.
+            specs : dict
+                The call specifications.
+            rng : RandomState instance
+                State of the random state at the end of minimization.
+
+    Note: This functions depends on helper_evaluate which acts as a black box to evaluate the
+    accuracy function based on a set of hyperparameters
+    """
+
+    function_to_minimize = lambda hyperparameters: helper_evaluate(
+        hyperparameters[0],
+        hyperparameters[1],
+        hyperparameters[2],
+        train_data,
+        val_data
+    )
+
+    return skopt.gp_minimize(function_to_minimize, search_space, n_calls=iterations)
+
+
 def main():
     train_matrix = load_train_sparse("./data").toarray()
     train_data = load_train_csv("./data")
     val_data = load_valid_csv("./data")
     test_data = load_public_test_csv("./data")
 
-    #####################################################################
-    #                                                          #
-    # (SVD) Try out at least 5 different k and select the best k        #
-    # using the validation set.                                         #
-    #####################################################################
-    arr = [1, 3, 5, 10, 20, 50, 100, 200, 500, 1000]
-    best_k = 0
-    highest_accuracy = 0.0
-    for k in arr:
+    theta = np.zeros(train_matrix.shape[0])
+    beta = np.zeros(train_matrix.shape[1])
+    mat = als_with_modification(train_data, 1000, 0.01, 100000, theta, beta)
+    acc = sparse_matrix_evaluate(val_data, mat)
+    print(acc)
+    mat = als(train_data, 1000, 0.01, 100000)
+    acc = sparse_matrix_evaluate(val_data, mat)
+    print(acc)
 
-        matrix = svd_reconstruct(train_matrix, k)
-        accuracy = sparse_matrix_evaluate(val_data, matrix)
-
-        print(f"k = {k}: Validation Accuracy = {accuracy:.4f}")
-
-        if accuracy > highest_accuracy:
-            highest_accuracy = accuracy
-            best_k = k
-
-    print(f"Best k selected based on validation accuracy is: {best_k}")
-    print(f"Highest validation accuracy: {highest_accuracy}")
-
-    final_reconstructed_matrix = svd_reconstruct(train_matrix, best_k)
-
-    final_test_accuracy = sparse_matrix_evaluate(test_data, final_reconstructed_matrix)
-
-    print(f"Final Test Accuracy with k={best_k}: {final_test_accuracy:.4f}")
-
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
-
-    #####################################################################
-
-    # (ALS) Try out at least 5 different k and select the best k        #
-    # using the validation set.                                         #
-    #####################################################################
-    k_values = [1, 3, 5, 10, 20, 50, 100, 200, 500, 1000, 2000]
-    learning_rates = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5]
-    iterations = [500, 1000, 5000, 8000, 10000, 15000]
-
-    best_k = None
-    best_lr = None
-    best_iter = None
-    best_val_acc = 0
-    best_train_acc = 0
-
-    for k in k_values:
-        for lr in learning_rates:
-            for num_iter in iterations:
-                mat = als(train_data, k, lr, num_iter)
-                acc = sparse_matrix_evaluate(val_data, mat)
-                train_acc = sparse_matrix_evaluate(train_data, mat)
-
-                if acc > best_val_acc:
-                    best_val_acc = acc
-                    best_k = k
-                    best_lr = lr
-                    best_iter = num_iter
-                if train_acc > best_train_acc:
-                    best_train_acc = train_acc
-
-    print("Learning the representations U and Z using ALS with SGD...")
-    print("---------------------------------------------------------")
-    print(f"Best k: {best_k}")
-    print(f"Best validation accuracy: {best_val_acc}")
-    print(f"Best training accuracy: {best_train_acc}")
-    print(f"Best learning rate: {best_lr}")
-    print(f"Best iterations: {best_iter}")
-    mat_best = (als(train_data,best_k, best_lr, best_iter))
-    acc_test = sparse_matrix_evaluate(test_data, mat_best)
-    print(f"Best test accuracy: {acc_test}")
-
-    mat, train_losses, val_losses = als_with_tracking(train_data, val_data, best_k, best_lr,
-                                                      best_iter)
-    n_train = len(train_data["is_correct"])
-    n_val = len(val_data["is_correct"])
-
-    train_losses = [loss / n_train for loss in train_losses]
-    val_losses = [loss / n_val for loss in val_losses]
-
-    track_every = 100
-    num_points = len(train_losses)
-    iterations = np.arange(1, num_points + 1) * track_every
-
-    plt.figure(figsize=(8, 5))
-    plt.plot(iterations, train_losses, label="Training Loss")
-    plt.plot(iterations, val_losses, label="Validation Loss")
-    plt.xlabel("Iterations")
-    plt.ylabel("Squared Error Loss")
-    plt.title("Training and Validation Loss vs Iterations")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+    # #####################################################################
+    # #                                                          #
+    # # (SVD) Try out at least 5 different k and select the best k        #
+    # # using the validation set.                                         #
+    # #####################################################################
+    # arr = [1, 3, 5, 10, 20, 50, 100, 200, 500, 1000]
+    # best_k = 0
+    # highest_accuracy = 0.0
+    # for k in arr:
+    #
+    #     matrix = svd_reconstruct(train_matrix, k)
+    #     accuracy = sparse_matrix_evaluate(val_data, matrix)
+    #
+    #     print(f"k = {k}: Validation Accuracy = {accuracy:.4f}")
+    #
+    #     if accuracy > highest_accuracy:
+    #         highest_accuracy = accuracy
+    #         best_k = k
+    #
+    # print(f"Best k selected based on validation accuracy is: {best_k}")
+    # print(f"Highest validation accuracy: {highest_accuracy}")
+    #
+    # final_reconstructed_matrix = svd_reconstruct(train_matrix, best_k)
+    #
+    # final_test_accuracy = sparse_matrix_evaluate(test_data, final_reconstructed_matrix)
+    #
+    # print(f"Final Test Accuracy with k={best_k}: {final_test_accuracy:.4f}")
+    #
+    # #####################################################################
+    # #                       END OF YOUR CODE                            #
+    # #####################################################################
+    #
+    # #####################################################################
+    #
+    # # (ALS) Try out at least 5 different k and select the best k        #
+    # # using the validation set.                                         #
+    # #####################################################################
+    # k_values = [1, 3, 5, 10, 20, 50, 100, 200, 500, 1000, 2000]
+    # learning_rates = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5]
+    # iterations = [500, 1000, 5000, 8000, 10000, 15000]
+    #
+    # best_k = None
+    # best_lr = None
+    # best_iter = None
+    # best_val_acc = 0
+    # best_train_acc = 0
+    #
+    # for k in k_values:
+    #     for lr in learning_rates:
+    #         for num_iter in iterations:
+    #             mat = als(train_data, k, lr, num_iter)
+    #             val_acc = sparse_matrix_evaluate(val_data, mat)
+    #             train_acc = sparse_matrix_evaluate(train_data, mat)
+    #
+    #             if val_acc > best_val_acc:
+    #                 best_val_acc = val_acc
+    #                 best_k = k
+    #                 best_lr = lr
+    #                 best_iter = num_iter
+    #             if train_acc > best_train_acc:
+    #                 best_train_acc = train_acc
+    #
+    # print("Learning the representations U and Z using ALS with SGD...")
+    # print("---------------------------------------------------------")
+    # print(f"Best k: {best_k}")
+    # print(f"Best learning rate: {best_lr}")
+    # print(f"Best iterations: {best_iter}")
+    # print(f"Best validation accuracy: {best_val_acc}")
+    # print(f"Best training accuracy: {best_train_acc}")
+    # mat_best = (als(train_data, best_k, best_lr, best_iter))
+    # acc_test = sparse_matrix_evaluate(test_data, mat_best)
+    # print(f"Best test accuracy: {acc_test}")
+    #
+    # mat, train_losses, val_losses = als_with_tracking(train_data, val_data, best_k, best_lr,
+    #                                                   best_iter)
+    # n_train = len(train_data["is_correct"])
+    # n_val = len(val_data["is_correct"])
+    #
+    # train_losses = [loss / n_train for loss in train_losses]
+    # val_losses = [loss / n_val for loss in val_losses]
+    #
+    # track_every = 100
+    # num_points = len(train_losses)
+    # iterations = np.arange(1, num_points + 1) * track_every
+    #
+    # plt.figure(figsize=(8, 5))
+    # plt.plot(iterations, train_losses, label="Training Loss")
+    # plt.plot(iterations, val_losses, label="Validation Loss")
+    # plt.xlabel("Iterations")
+    # plt.ylabel("Squared Error Loss")
+    # plt.title("Training and Validation Loss vs Iterations")
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
+    #
+    # # #####################################################################
+    # # #                       END OF YOUR CODE                            #
+    # # #####################################################################
+    # #
+    # # #####################################################################
+    # # # In this part we try to optimize hyperparameters using our         #
+    # # # find_optimal_hyperparameters function we will try 1000 iterations #
+    # # #####################################################################
+    # # # First create our search space
+    # search_space = [
+    #     Integer(1, 1000, name='k'),
+    #     Real(0.0001, 0.3, name='lr'),  # Keep at less than 0.3 or becomes unstable
+    #     Integer(1, 1000, name='num_iter'),
+    # ]
+    # # Now call our function
+    # result = find_optimal_hyperparameters(search_space, 100, train_data, val_data)
+    #
+    # print(f"Optimal hyperparameters: k={result.x[0]}, lr={result.x[1]}, num_iter={result.x[2]}")
+    # print(f"Best score achieved: {-result.fun}")
+    #
+    # # Plot the convergence
+    # _ = plot_convergence(result)
+    # plt.show()
+    #
+    # fig = plt.figure(figsize=(10, 8))
+    #
+    # _ = plot_evaluations(result, ax=fig.gca())
+    #
+    # for ax in fig.get_axes():
+    #     for label in ax.get_yticklabels():
+    #         label.set_rotation(45)
+    #
+    # plt.tight_layout()
+    # plt.show()
+    #
+    # # mat = als(train_data, 10000, 0.01, 3000000)
+    # # val_acc = sparse_matrix_evaluate(val_data, mat)
+    # # print(val_acc)
 
 
 if __name__ == "__main__":
